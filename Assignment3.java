36f679a4078abc3b01f9eb425f4915d0bfbc4128
Frank Le <frank.le@utexas.edu>
2020-05-02 18:49:07 -0500

    added starter code

---
+++ Assignment3.java
@@ -0,0 +1,8 @@
+public class Assignment3 {
+
+    public int maxFruitCount (int[] sections) {
+        // Implement your dynamic programming algorithm here
+        // You may use helper functions as needed
+        return 0;
+    }
+}
